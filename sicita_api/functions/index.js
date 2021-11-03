e95b43a8069bce2c3fde56cdacf75e6abd0ecfcf
Giovanny <geovannymauricio@gmail.com>
2021-11-02 22:48:43 -0500

    02112021

---
+++ sicita_api/functions/index.js
@@ -0,0 +1,19 @@
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sicita-8e3c0-default-rtdb.firebaseio.com"
+});
+
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
