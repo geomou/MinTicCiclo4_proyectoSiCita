a3d7c57a7bba111380baf0cbcb839e7bfa7e298c
Giovanny <geovannymauricio@gmail.com>
2021-11-14 17:05:54 -0500

    14112021_1705

--- SiCita/app/src/main/java/com/example/sicita/Asesor.java
+++ SiCita/app/src/main/java/com/example/sicita/Asesor.java
@@ -6,13 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class Asesor extends AppCompatActivity {
+    private EditText txtid;EditText txtidentificacion;EditText txtnombre;EditText txttelefono; EditText txtmovil;EditText txtemail;
+    Switch swmg; Switch swpintura;Switch swlatoneria;Switch swelectoespe;Switch swelectrogral;
+    Button btnactualizar;Button btnborrar;
+    String dato="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asesor);
+        txtid=(EditText) findViewById(R.id.idtxtid);
+        txtidentificacion=(EditText) findViewById(R.id.idtxtidentificacion);
+        txtnombre=(EditText) findViewById(R.id.idtxtnombre);
+        txttelefono=(EditText)  findViewById(R.id.iditxttelefono);
+        txtmovil=(EditText) findViewById(R.id.idtxtmovil);
+        txtemail=(EditText) findViewById(R.id.idtxtemail);
+        swmg=(Switch) findViewById(R.id.idswmg);
+        swpintura=(Switch) findViewById(R.id.idswpintura);
+        swlatoneria=(Switch) findViewById(R.id.idswlatoneria);
+        swelectoespe=(Switch) findViewById(R.id.idswelectroespecial);
+        swelectrogral=(Switch) findViewById(R.id.idswelectrogeneral);
+        swelectoespe=(Switch) findViewById(R.id.idswelectroespecial);
+        btnactualizar =(Button) findViewById(R.id.idbtnActualizar);
+        btnborrar=(Button) findViewById(R.id.idbtnborrar);
+
     }
     @Override
     public  boolean onCreateOptionsMenu(Menu menu)
@@ -119,5 +143,50 @@ public class Asesor extends AppCompatActivity {
         return super.onOptionsItemSelected(menuItem);
     }
 
+    public void Actualizar(View view)
+    {
+        String id=txtid.getText().toString();
 
+        boolean validar= this.validarCampos();
+        Toast.makeText(this, "No por admin  " + validar, Toast.LENGTH_SHORT).show();
+        if (!validar.)
+        {
+            Toast.makeText(this, "No es posible almacenar , por falta de la siguiente información " + dato, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public boolean validarCampos()
+    {
+
+       boolean continuar=true;
+
+       if (txtidentificacion.equals(""))
+       {
+           dato=dato+ " Identificación";
+           continuar= false;
+       }
+       if (txtnombre.equals(""))
+       {
+           dato=dato+ " Nombre ";
+           continuar= false;
+       }
+        if (txttelefono.equals(""))
+        {
+            dato=dato+ " Teléfono  ";
+            continuar= false;
+        }
+        if (txtmovil.equals(""))
+        {
+            dato=dato+ " Teléfono Móvil  ";
+            continuar= false;
+        }
+        if (txtemail.equals(""))
+        {
+            dato=dato+ " Email  ";
+            continuar= false;
+        }
+        Toast.makeText(this, "entro" + dato + continuar, Toast.LENGTH_SHORT).show();
+        return continuar;
+    }
 }
--- SiCita/app/src/main/java/com/example/sicita/login.java
+++ SiCita/app/src/main/java/com/example/sicita/login.java
@@ -8,9 +8,12 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import  android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    private Button btoingresar; EditText txtusuario; EditText txtclave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,20 +23,61 @@ public class Login extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
-
-        //Ocultar Accion bar
+                //Ocultar Accion bar
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
+
+        txtusuario=(EditText) findViewById(R.id.idtxtemailusuairo);
+        txtclave=(EditText) findViewById(R.id.idtxtclave);
+        btoingresar=(Button) findViewById(R.id.idbtningresar);
+
+
+
     }
 
 
-    public void goToActivity (View view) {
 
-        Intent newIntent =new Intent( this,Principal.class);
 
-        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(newIntent);
-        finish();
+    public void ingresar(View view)
+    {
+        String usuario= txtusuario.getText().toString();
+        String clave=txtclave.getText().toString();
+
+        if (!usuario.equals("") && !clave.equals(""))
+        {
+        if (usuario.equals("admin") && clave.equals("admin"))
+        {
+            Intent newIntent =new Intent( this,Principal.class);
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(newIntent);
+            finish();
+
+        }
+        else {
+            Toast.makeText(this, "Usuario o clave Incorrecto", Toast.LENGTH_SHORT).show();
+            txtusuario.requestFocus();
+        }
+
+        }
+        else
+        {
+            if (usuario.equals(""))
+            {
+                Toast.makeText(this, "Usuario no contiene información ", Toast.LENGTH_SHORT).show();
+                txtusuario.requestFocus();
+            }
+            if (clave.equals(""))
+            {
+                Toast.makeText(this, "Clave no contiene información ", Toast.LENGTH_SHORT).show();
+                txtclave.requestFocus();
+            }
+
+        }
+
+
+
+
+
 
     }
 }
