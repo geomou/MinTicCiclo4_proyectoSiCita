93ffbf30238989b9e43c7b88c485eddc473b68e9
Giovanny <geovannymauricio@gmail.com>
2021-11-28 06:16:57 -0500

    Merge remote-tracking branch 'origin/desarrollador1' into desarrollador1

--- GlobalSICITA/app/src/main/java/com/example/globalsicita/Login.java
+++ GlobalSICITA/app/src/main/java/com/example/globalsicita/Login.java
@@ -2,6 +2,7 @@ package com.example.globalsicita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ Button btningresar;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         txtusurio=(EditText) findViewById(R.id.idtxtemailusuairo);
         txtclave=(EditText) findViewById(R.id.idtxtclave);
