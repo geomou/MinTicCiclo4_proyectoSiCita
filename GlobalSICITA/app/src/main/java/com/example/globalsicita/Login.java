2c48061cb90c353f47bcb3826eaebe14781d271f
Giovanny <geovannymauricio@gmail.com>
2021-11-28 06:10:06 -0500

    28112021

--- GlobalSICITA/app/src/main/java/com/example/globalsicita/Login.java
+++ GlobalSICITA/app/src/main/java/com/example/globalsicita/Login.java
@@ -3,12 +3,23 @@ package com.example.globalsicita;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+String usuario, clave;
+EditText txtusurio, txtclave;
+Button btningresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtusurio=(EditText) findViewById(R.id.idtxtemailusuairo);
+        txtclave=(EditText) findViewById(R.id.idtxtclave);
+        btningresar=(Button) findViewById(R.id.idbtningresar);
+
+
+
     }
 }
